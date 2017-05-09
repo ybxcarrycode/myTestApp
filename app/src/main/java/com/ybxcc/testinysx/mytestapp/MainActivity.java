539e772e267bfcbb6ad7d869f9b4b039dcec18aa
ybx <834718102@qq.com>
2017-05-09 17:32:21 +0800

    修改测试——1

--- app/src/main/java/com/ybxcc/testinysx/mytestapp/MainActivity.java
+++ app/src/main/java/com/ybxcc/testinysx/mytestapp/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private Toast toast = null;
-    
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +31,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
 
